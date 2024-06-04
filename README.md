3f87b99ad1f6184d2a706720605bace1f380031d
Mino <kyung425@gmail.com>
2024-06-04 11:31:18 +0900

    add partialy supports .proto, less print folders and bug fix

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # MINIPUB
 
-miniPublisher for obsidian: convert .md files to .html including obsidian tags and more.
+MiniPub is a mini html pubulisher for obsidian: converts .md files to .html including obsidian tags and more processing.
+It supports import another .md files and make .md file from source code like java, c# and etc.
+
+This one in on developing.
 
 # OBSIDIAN TAGS
 
 ## ```[[name]]```
 
-
 ## ```![[img]]```
-
 
 # TITLE NUMBERING
 
@@ -20,8 +21,7 @@ miniPublisher for obsidian: convert .md files to .html including obsidian tags a
 
 ## @import-embedded
 
-
-
 # TODO
 
 - callout: https://docs.readme.com/rdmd/docs/callouts
+- figure and table numbering
--- miniPub.py
+++ miniPub.py
@@ -6,13 +6,13 @@
 # TODO: 
 # - read YFM and apply scoped configure.
 # - add CallOut
+# - add .proto
 ## markdown@ 
         
 import unittest
 import argparse
 import markdown2
 import os
-import json
 import re
 import yaml
 
@@ -48,10 +48,27 @@ class ReMarkdown:
         self.re_import = re.compile(r'^(#{1,5})\s(@import)\s*\[\[(.*)\]\]')
         self.re_link = re.compile(r'(.*)(@link)\s*\[\[(.*)\]\]\s*\((.*)\)')    # ------
         self.re_number_level = re.compile(r'^(#{1,5})\s+(.*)')
-        self.re_markdown_begin = re.compile(r'^(\s*)(\/\*\*\s@markdown)')
-        self.re_markdown_end   = re.compile(r'^(\s*)(@markdown \*\*\/)')
         self.re_obsidian_image = re.compile(r'(.*)!\[\[(.*)\]\]')
         self.re_obsidian_link = re.compile(r'(.*)\s*\[\[(.*)\]\]')
+
+        self.re_markdown_begin = re.compile(r'^(\s*)(\/\*\*\s@markdown)')
+        self.re_markdown_end  = re.compile(r'^(\s*)(@markdown \*\*\/)')    
+        self.re_markdown_line = re.compile(r'^\s*\/{3}\s+(.*)$')            # (.*)(###|\/\/\/)\s(.*)$')
+    
+        self.re_proto_package  = re.compile(r'^package\s+(.*)\s*;')
+            # package CPH;
+        self.re_proto_services  = re.compile(r'^service\s+(\w+)\s+[{|\s+]\s+\/{3}\s?(.*)$')
+            # service CPH {           /// service desc
+        self.re_proto_service  = re.compile(r'\s?rpc\s+(\w+)\s*\((\w+)\)\s+returns\s+\((\w+)\)\s?\;?\s+\/{3}\s(.*)$')
+            # rpc UploadFile (FileUploadRequest) returns (FileUploadResponse)	/// File Upload to CPH and move
+            # rpc UploadFile (FileUploadRequest) returns (FileUploadResponse);   /// File Upload to CPH and move
+        self.re_proto_messages  = re.compile(r'message\s+(\w+)\s+[{|\s+]\s+\/{3}\s?(.*)$')
+            # message doReq {           /// msg desc 
+        self.re_proto_message   = re.compile(r'\s?(\w+)\s+(\w+)\s?=\s?(\d+)\s?;\s+\/{3}\s?(.*)$')
+            #   string pacid = 1;       /// 'CPH'
+            #   string serviceid = 2;   /// 'name of service'
+            #   string reserved = 3;    /// reserved string in the future
+            #   bytes serialized = 4;    /// TODO: +++ CHECK LENGTH LIMIT                                             
 
     def proc_link(self, line, line_num) -> str:
         """ # @link [[도움말_요약]](../04. user manual/도움말_요약.md) """
@@ -86,13 +103,13 @@ class ReMarkdown:
         link = os.path.relpath(get_link(img), os.getcwd())
         line = f'![]({link})\n'
         if os.path.isfile(link):
-            print(f'      ob_img: {link}:{line_num}')
+            print(f'      ob_img: "{link}":{line_num}')
         else:
             warn( 'file not found: ', link)
         return line
 
     def proc_import(self, line, line_num, depth=0) -> str:
-        """ # @import [[도움말_요약]] """
+        """ # @import [[help_summary]] """
         m = self.re_import.match(line)
         level_bias = m[1]
         md = m[3]
@@ -111,32 +128,116 @@ class ReMarkdown:
         level_bias = m[1]
         src = m[3]
         import_src = os.path.relpath(src, os.getcwd())
-        import_md = os.path.join(tempfile.mkdtemp(), '__temp__.md')
+        import_md = os.path.join(tempfile.mkdtemp(), os.path.basename(import_src)+'.md')
         if not os.path.exists(import_src):
             warn('file not found: ', f'{os.cwd()}:{import_src}')
-        self.gen_embed2md(import_src, import_md)
+
+        # CHECK extention here ++++  cs, c, cpp, java, NOT py yet
+        if re.search(r'(.proto$)', import_src):
+            self.gen_embed_proto2md(import_src, import_md)
+        else:
+            self.gen_embed_genneral2md(import_src, import_md)
+
         try:
             print(f'      import-emb: {src} -> "{import_md}": {line_num}')
             line = self.get_md(import_md, depth+1, level_bias)
             if len(line) == 0:
                 warn('EMPTY embedded: ', import_md)
                 line = f'WARN EMPTY embedded: {import_md}'
-            os.remove(import_md)
+            # os.remove(import_md)
         except:
             warn( 'file not found: ', import_md) 
         return line
 
-    def gen_embed2md(self, src, md) -> str:
+    def gen_embed_proto2md(self, src, md):
         """
-        Generate a tempory markdown from Embeded files like ended with .cs, .cpp, .java. 
+        Generate a tempory markdown from proto. !!! LIMITED flat file only !!!!
+        """
+        markdown = False
+        tab_size = options['do']['tab-size']
+        left_spaces = 0
+        with open(src, 'r', encoding='utf-8') as f, open(md, 'w', encoding='utf-8') as w:
+            w.write('\n')
+            for line in f:
+                # service block
+                m = self.re_proto_services.search(line)
+                if m:
+                    w.write(f'\n## Service {m[1]}\n\n')
+                    w.write(f'{m[2]}\n\n')
+                    # if need more descrion +++
+                    th = f'|end point|desc|\n'
+                    th += f'|---------|----|\n'
+                    # w.write(')
+                    # w.write(f'|---------|----|\n')
+                m = self.re_proto_service.search(line)
+                if m:
+                    if th:
+                        w.write(th)
+                        th = ''
+                    anker = f'{pkg_name}_{m[2].lower()}'
+                    req = f'<a href="#{anker}">{m[2]}</a>'
+                    anker = f'{pkg_name}_{m[3].lower()}'
+                    res = f'<a href="#{anker}">{m[3]}</a>'
+                    if m[4] == '{}':
+                        w.write(f'| {m[1]} ({req}, {res}) | {m[5]}|\n')
+                    else:
+                        w.write(f'| {m[1]} ({req}, {res}) | {m[4]}|\n')
+
+                # message block
+                m = self.re_proto_messages.search(line)
+                if m:
+                    anker = f'{pkg_name}_{m[1].lower()}'
+                    w.write(f'<p id="{anker}"></p>')
+                    w.write(f'\n## {m[1]}\n\n')
+                    w.write(f'{m[2]}\n\n')
+                    # if need more descrion +++
+                    # w.write(f'|seq|type|name|desc|\n')
+                    # w.write(f'|:-:|----|----|----|\n')
+                    th = f'|seq|type|name|desc|\n'
+                    th += f'|:-:|----|----|----|\n'
+                m = self.re_proto_message.search(line)
+                if m:
+                    if th:
+                        w.write(th)
+                        th = ''
+                    w.write(f'|{m[3]}|{m[1]}|{m[2]}|{m[4]}|\n')
+
+                # markdown block - SHOULD BE DEPRICATED
+                # if self.re_markdown_end.match(line):
+                #     markdown = False
+                # if markdown == True:
+                #     line = line.replace('\t', ' '*int(tab_size))
+                #     line = line[left_spaces:512]   # ++++ LINE SEP ????
+                #     if len(line) == 0:
+                #         line = os.linesep
+                #     w.write(line)
+                # if self.re_markdown_begin.match(line):
+                #     m = self.re_markdown_begin.match(line)
+                #     left_spaces = len(m[1].replace('\t', ' '*4))
+                #     markdown = True
+             
+                # markdown line
+                m = self.re_markdown_line.search(line)
+                if m:
+                    w.write(f'{m[1]}\n')
+
+                m = self.re_proto_package.search(line)
+                if m:
+                    pkg_name = m[1]
+                    w.write(f'\n# {pkg_name}\n')                    
+    
+    def gen_embed_genneral2md(self, src, md) -> str:
+        """
+        Generate a tempory markdown from embeded files like ended with .cs, .cpp, .java. 
         Files applied with left spaces as its left trimming.
         """
         markdown = False
         tab_size = options['do']['tab-size']
         left_spaces = 0
-        # CHECK extention here ++++  cs, c, cpp, java, NOT py yet
         with open(src, 'r', encoding='utf-8') as f, open(md, 'w', encoding='utf-8') as w:
             for line in f:
+
+                # # markdown block  --- SHOULD BE DEPRICATED
                 if self.re_markdown_end.match(line):
                     markdown = False
                 if markdown == True:
@@ -149,6 +250,12 @@ class ReMarkdown:
                     m = self.re_markdown_begin.match(line)
                     left_spaces = len(m[1].replace('\t', ' '*4))
                     markdown = True
+
+                # markdown line
+                m = self.re_markdown_line.search(line)
+                if m:
+                    w.write(f'{m[1]}\n')
+
         return line
             
     def get_md(self, md, depth = 0, bias = '#') -> str:
@@ -162,6 +269,7 @@ class ReMarkdown:
             f = open(md, encoding='utf8')
             for line in f:
                 if self.re_import_embdded.match(line):
+                    lines += f'<!-- {line[:-1]} -->\n'
                     line = self.proc_embeded(line, line_num, depth)
 
                 if self.re_import.match(line):
@@ -183,9 +291,11 @@ class ReMarkdown:
                 line_num = line_num + 1
             f.close()
             return lines
-        except:
+        except FileNotFoundError:
             warn( 'File not found: ', md)
             return f'File not found: "{md}"\n'
+        except Exception as e:
+            warn( f'Exception {e}: ', md)
         
     def clear_levels(self):
         """ clear all level counter """
@@ -223,11 +333,7 @@ class ReMarkdown:
         return return_str + '.'        
 
 def asian_pad(input_s, max_size=40, fill_char=' '):
-    """
-    - 길이가 긴 문자는 2칸으로 체크하고, 짧으면 1칸으로 체크함. 
-    - 최대 길이(max_size)는 40이며, input_s의 실제 길이가 이보다 짧으면 남은 문자를 fill_char로 채운다.
-    https://frhyme.github.io/python-libs/print_kor_and_en_full_half_width/
-    """
+    """ https://frhyme.github.io/python-libs/print_kor_and_en_full_half_width/ """
     l = 0 
     for c in input_s:
         if unicodedata.east_asian_width(c) in ['F', 'W']:
@@ -278,7 +384,17 @@ def get_link(key):
     if key in links:
         return links[key]
     else:
-        return f'"{key}"'
+        return f'{key}'
+    
+def get_toc(md):    
+    """ extract ToC from markdown text """
+    headers = re.findall(r'\n#{1,5}\s*(.*)', md)
+    toc = []
+    for header in headers:
+        header_text = f'{header}'
+        anchor = header_text.strip().lower().replace('/', '').replace(' ', '-').replace('.', '')
+        toc.append(f'- [{header_text}](#{anchor})')
+    return '\n'.join(toc)    
 
 remark = ReMarkdown()
 
@@ -290,6 +406,8 @@ def convert_markdown_to_html(source_file, build_file):
 
     remark.clear_levels()
     markdown_text = remark.get_md(source_file)
+
+    # write a file for debugging
     with open(build_file + '.markdown', 'w', encoding='utf-8') as f:
          f.write(markdown_text)
 
@@ -303,7 +421,7 @@ def convert_markdown_to_html(source_file, build_file):
       <title> {title} </title>
       <link rel="stylesheet" href="{css}" />
     </head>
-    <body>    
+    <body>
     '''
 
     tailer = r'''
@@ -311,19 +429,17 @@ def convert_markdown_to_html(source_file, build_file):
     </html>
     '''
 
-    converter = markdown2.Markdown(extras=['tables', 'footnotes', 'fenced-code-blocks', 'header-ids', 'strike'])  
+    html_content = markdown2.markdown(markdown_text, extras=['tables', 'footnotes', 'toc', 'fenced-code-blocks', 'header-ids', 'strike'])  
       # use-file-vars
-    html_content = converter.convert(markdown_text)
+    toc_html = html_content.toc_html
+    if toc_html != None:
+        html_content = html_content.replace('[TOC]', toc_html)         
+
     os.makedirs(os.path.dirname(build_file), exist_ok=True)
     with open(build_file, 'w', encoding='utf-8') as f:
         f.write(header)
         f.write(html_content)
         f.write(tailer)
-
-    #  --toc --toc-depth=5
-    # opt = '--css=../css/markdown7.css'
-    # os.system(f'pandoc "{source_file}.markdown" -o "{build_file}" -f markdown {opt} --standalone --metadata pagetitle="{build_file}"')
-    # os.remove
 
 base_build = ''
 base_css=''
@@ -342,7 +458,7 @@ def build(source, build):
     for root, _, files in os.walk(root):
         if any(word in root for word in ignores):
             continue
-        print(f'  {bg.OKCMD}{root}{bg.ENDC}')        
+        working_folder = f'  {bg.OKCMD}{root}{bg.ENDC}'
         os.chdir(root)
         for file in files:
             if file.endswith('.md'):
@@ -351,15 +467,21 @@ def build(source, build):
                 os.makedirs(build_path, exist_ok=True)             
                 build_file = os.path.join(build_path, file.replace('.md', '.html'))
                 if not os.path.exists(build_file) or os.path.getmtime(source_file) >= os.path.getmtime(build_file):
-                    print(f'    {bg.OKFILE}convert: {source_file}{bg.ENDC}')
+                    if working_folder:
+                        print(working_folder)
+                        working_folder = ''
+                    print(f'    {bg.OKFILE}{source_file}{bg.ENDC}')
                     convert_markdown_to_html(source_file, build_file)
 
 # CLEAN
 def clean(folder):
     for root, _, files in os.walk(folder):
-        print(f'  {bg.OKCMD}{root}{bg.ENDC}')
+        working_folder = f'    {bg.OKCMD}{root}{bg.ENDC}'
         for file in files:
             if file.endswith(('.html', '.mark', '.markdown')):
+                if working_folder:
+                    print(working_folder)
+                    working_folder = ''
                 file_path = os.path.join(root, file)
                 print(f'    cleaned: {file_path}')
                 os.remove(file_path)
@@ -412,21 +534,22 @@ class Test(unittest.TestCase):
         self.assertTrue(True, True)        
 
 if __name__ == '__main__':
-    with open('miniPub.yml') as f:
+    cwd = os.path.dirname(__file__)
+    with open(f'{cwd}/miniPub.yml') as f:
         options = yaml.load(f, Loader=yaml.FullLoader)
-        print('Options:\n', yaml.dump(options, default_flow_style=False))
+        # print('Options:\n', yaml.dump(options, default_flow_style=False))
 
     # parse args
     parser = argparse.ArgumentParser(description="Convert Markdown files to HTML")
-    parser.add_argument("action", choices=['build', 'clean', 'deploy', 'test'], 
+    parser.add_argument("command", choices=['build', 'clean', 'deploy', 'test'], 
                         help="Action to perform: 'build' to convert Markdown to HTML,"
                         + " 'clean' to delete the build directory")
     parser.add_argument("--source", default=options['folders']['source'], help="source directory containing Markdown files")
     parser.add_argument("--build", default=options['folders']['build'], help="build directory to save HTML files")
     args = parser.parse_args()
     try:
-        print(f'\n# {args.action}\n')
-        if args.action == 'build':
+        print(f'\n# {args.command}\n')
+        if args.command == 'build':
             pushd = os.getcwd()
             # if not args.source or not args.build:
             #     parser.error("Please provide both source and build directories.")
@@ -435,25 +558,24 @@ if __name__ == '__main__':
             # phase 2
             build(args.source, args.build)
             os.chdir(pushd)
-            file = 'unused.csv'
-            if os.path.exists(file):
-                os.remove(file)
+            unused = '.unused.csv'
+            if os.path.exists(unused):
+                os.remove(unused)
             if(len(links_unused) > 0):
                 warn('Check: ', 'Unused links')
-                print(f'Unused Dic: {len(links_unused)} / {len(links)}, Check {file}')
+                print(f'Unused Dic: {len(links_unused)} / {len(links)}, Check {unused}')
                 pretty_dics(links_unused)
-                with open('unused.csv','w', encoding='utf-8-sig') as f:
+                with open(unused,'w', encoding='utf-8-sig') as f:
                     for key, value in links_unused.items():
                         f.write(f'{value}, {key}\n')
-        elif args.action == 'clean':
+        elif args.command == 'clean':
             clean(args.build)
-        elif args.action == 'test':
+        elif args.command == 'test':
             unittest.main()
-        elif args.action == 'deploy':
+        elif args.command == 'deploy':
             deploy()
         else:
             parser.usage()
     except Exception as e:
-            print(e)
-            print(f'{bg.FAIL} This sw may has some problem.{bg.ENDC}')
+           print(f'{bg.FAIL} This sw may has some problem: {e}.{bg.ENDC}')
     print('\n# finished.')
